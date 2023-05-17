2aefb002c175c9e058273582cb26523d20696041
tengis4262 <e.tengis@gmail.com>
2023-05-16 22:51:44 -0500

    changes

---
+++ Day5/Assignment/Exercise2.js
@@ -0,0 +1,41 @@
+class Subject {
+    constructor() {
+        this.observers = {};
+    }
+
+    on(event, fn) {
+        if (!this.observers[event]) {
+            this.observers[event] = [];
+        }
+        this.observers[event].push(fn);
+    }
+
+    emit(event, message) {
+        const observers = this.observers[event] || [];
+        observers.forEach(observer => observer(message));
+    }
+}
+
+
+
+
+
+
+const subject = new Subject();
+subject.on('eat', console.log); // register an observer
+subject.on('study', console.log); // register an observer
+
+function foo(msg) {
+    console.log('foo: ' + msg);
+}
+subject.on('eat', foo);
+subject.on('study', foo);
+
+subject.emit('eat', 'Corn');
+//output for Line above: subject.emit('eat', 'Corn');
+// Corn
+// foo: Corn
+subject.emit('study', 'cs445');
+//output for Line above: subject.emit('study', 'cs445');
+// cs445
+// foo: cs445
